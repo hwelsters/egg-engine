69092a40af84fefe7cd8784a675749730935eee9
hwelsters <corevalue.noel@gmail.com>
2023-12-19 00:57:00 -0700

    feat: added class and macros for logging.

--- src/core/entrypoint.h
+++ src/core/entrypoint.h
@@ -5,13 +5,21 @@
 #define EGG_CORE_ENTRYPOINT_H
 
 #include "core/application.h"
+#include "core/log.h"
 
 extern egg::Application* egg::CreateApplication();
 
 int main(int, char*[]) {
+  egg::Log::Init();
+  egg::Log::GetCoreLogger()->warn("Let's get this core party started!");
+  egg::Log::GetClientLogger()->info("Let's get this client party started!");
+
   egg::Application* application = egg::CreateApplication();
   application->Run();
   delete application;
 }
+
+#define EGG_CORE_TRACE(...) egg::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define EGG_CORE_TRACE(...) egg::Log::GetCoreLogger()->trace(__VA_ARGS__)
 
 #endif
